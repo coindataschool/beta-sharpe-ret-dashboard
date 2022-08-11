0219d60b58c3bd7461a6cd0f7cb489c1031ed51c
Coin Data School <coindataschool@gmail.com>
2022-08-11 13:03:01 +0900

    test

--- main.py
+++ main.py
@@ -61,7 +61,8 @@ df_prices = df_prices.iloc[:-1]
 # these rates are already multiplied by 100, so we divide them by 100 to 
 # make them on the same scale as the returns we will calculate. 
 rfs = reader.DataReader('F-F_Research_Data_Factors', 'famafrench', start, end)[0].RF / 100
-st.dataframe(rfs.head(3))
+
+st.dataframe(rfs.index[:3])
 
 # calculate monthly returns 
 # because `df_prices`` includes price for the day before `start`, we use 
